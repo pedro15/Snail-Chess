e2858f9d4f3dc776055d6cbd32759a039086d02b
Pedro Duran <pedrojosuedm@hotmail.com>
2024-09-29 14:01:47 -0400

    updated tuner_data file location

--- Scripts/tune.bat
+++ Scripts/tune.bat
@@ -1,5 +1,5 @@
 SET ARG="%1"
-SET SRC_FILE="%~dp0..\Snail-Chess.Tuner\data\tuner_data.epd"
+SET SRC_FILE="%~dp0..\Data\tuner_data.epd"
 if %ARG% == "-c" (
 ECHO "Copying tuner data..."
 COPY /A %SRC_FILE% "%~dp0..\Snail-Chess.Tuner\bin\Release\net7.0\dataset.epd"
